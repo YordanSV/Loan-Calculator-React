349ce887cdbbec7082fbac862404874748e11086
YordanSV <yordan10_pv@hotmail.com>
2024-03-20 20:47:20 -0600

    Add buttons for increments

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,46 @@ function App() {
     setAmount(+e.target.value)
   }
 
+  function handleClickDecrement() {
+    const value = amount - step;
+    if (value < min) {
+      alert('Invalid amount');
+      return;
+    }
+
+    setAmount(value);
+  }
+
+  function handleClickIncrement() {
+    const value = amount + step;
+    if (value > max) {
+      alert('Invalid amount');
+      return;
+    }
+
+    setAmount(value);
+  }
   return (
     <div className='my-20 max-w-lg mx-auto bg-white shadow p-10'>
       <Header />
 
+      <div className="flex justify-between my-6">
+        <button
+          type="button"
+          className="h-10 w-10 flex items-center justify-center font-bold text-white
+          text-2xl bg-lime-500 rounded-full hover:outline-none hover:ring-2 
+          hover:ring-offset-2 hover:ring-lime-500"
+          onClick={handleClickDecrement}
+        >-</button>
+
+        <button
+          type="button"
+          className="h-10 w-10 flex items-center justify-center font-bold text-white
+          text-2xl bg-lime-500 rounded-full hover:outline-none hover:ring-2 
+          hover:ring-offset-2 hover:ring-lime-500"
+          onClick={handleClickIncrement}
+        >+</button>
+      </div>
       <input
         type="range"
         className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"

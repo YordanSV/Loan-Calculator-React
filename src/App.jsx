11595d9e2480346ff45d43ad4df6e2b8a3df8adb
YordanSV <yordan10_pv@hotmail.com>
2024-03-20 20:31:44 -0600

    Add variables for Minimum, Maximum and Step

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Header from "./components/loan_calculator/Header"
 
 function App() {
   const [amount, setAmount] = useState(10000);
+  const min = 0;
+  const max = 20000;
+  const step = 100;
 
   function handleChange(e) {
     setAmount(+e.target.value)
@@ -16,6 +19,10 @@ function App() {
         type="range"
         className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
         onChange={handleChange}
+        min={min}
+        max={max}
+        step={step}
+        value={amount}
       />
 
       <p className="text-center my-10 text-5xl font-extrabold text-indigo-600">{amount}</p>

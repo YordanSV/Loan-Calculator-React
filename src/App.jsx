28f9d0af531cef084bd68abf929a261fadc91e43
YordanSV <yordan10_pv@hotmail.com>
2024-03-20 20:25:01 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+import Header from "./components/loan_calculator/Header"
+
+function App() {
+  const [amount, setAmount] = useState(10000);
+
+  function handleChange(e) {
+    setAmount(+e.target.value)
+  }
+
+  return (
+    <div className='my-20 max-w-lg mx-auto bg-white shadow p-10'>
+      <Header />
+
+      <input
+        type="range"
+        className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+        onChange={handleChange}
+      />
+
+      <p className="text-center my-10 text-5xl font-extrabold text-indigo-600">{amount}</p>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/loan_calculator/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return(
+        <h1 className='text-4xl font-extrabold text-gray-500 text-center'>
+        When <span className='text-indigo-600'>money</span> will you need?
+        </h1>
+    )
+}
+
+export default Header

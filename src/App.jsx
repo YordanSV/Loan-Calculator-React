76228f8a83cf529a70b8f600a8b00dad70748eb0
YordanSV <yordan10_pv@hotmail.com>
2024-03-21 08:26:15 -0600

    Creating the payment Summary

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Button from "./components/loan_calculator/Button";
 import { formatMoney } from "./helpers";
 function App() {
   const [amount, setAmount] = useState(10000);
-  const [ months, setMonths] = useState(6)
+  const [months, setMonths] = useState(6)
   const min = 0;
   const max = 20000;
   const step = 100;
@@ -66,12 +66,22 @@ function App() {
 
       <select className="mt-5 w-full p-2 bg-white border border-gray-300 rounded-lg text-center
       text-xl font-bold text-gray-500"
-      value={6}
-      onChange={e => setMonths(+e.target.value)}>
+        value={6}
+        onChange={e => setMonths(+e.target.value)}>
         <option value="6">6 months</option>
         <option value="12">12 months</option>
         <option value="24">24 months</option>
       </select>
+
+      <div className="my-5 space-x-3 bg-gray-50 p-5">
+        <h2 className="text-2xl font-extrabold text-gray-500 text-center">
+          <span className="text-indigo-600">Payment</span> summary
+        </h2>
+
+        <p className="text-xl text-gray-500 text-center font-bold">{months} Months</p>
+        <p className="text-xl text-gray-500 text-center font-bold">Total to pay</p>
+        <p className="text-xl text-gray-500 text-center font-bold">Monthly payments</p>
+      </div>
     </div>
   )
 }
